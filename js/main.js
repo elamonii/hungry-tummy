6c5101590a56894e1587aa769c60b80adcc8116c
elamonii <75824927+elamonii@users.noreply.github.com>
2022-08-29 09:42:39 +0600

    first commit

--- js/main.js
+++ js/main.js
@@ -1,28 +1,39 @@
 
-const loadMeal = () =>{
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata`
+// const loadMeal = () =>{
+//     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata`
+//     fetch(url)
+//     .then(response => response.json())
+//     .then(data => loadMealsIntoDiv(data.meals[0]));
+// }
+
+
+const loadMeal = () => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?f=b`
     fetch(url)
-    .then(response => response.json())
-    .then(data => loadMealsIntoDiv(data.meals[0]));
+    .then(res => res.json())
+    .then(data => loadMealsIntoDiv(data.meals))
 }
 
-const loadMealsIntoDiv = meal =>{
+
+
+const loadMealsIntoDiv = mealAll =>{
     const getContainer = document.getElementById('food-container');
     getContainer.innerHTML = '';
-    const makeCard = document.createElement('div');
-    makeCard.classList.add('col');
-    makeCard.innerHTML = `
-
+    console.log(mealAll);
+    mealAll.forEach(meal =>{
+        const makeCard = document.createElement('div');
+        makeCard.classList.add('col');
+        makeCard.innerHTML = `
             <div class="card">
                 <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${meal.strMeal}</h5>
-                    <p class="card-text">${meal.strInstructions}</p>
+                    <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
                 </div>
             </div>
-    
-    `;
-    getContainer.appendChild(makeCard);
+        `;
+        getContainer.appendChild(makeCard);
+    })
 
 
 }
